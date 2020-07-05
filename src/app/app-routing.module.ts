909b4d24aa33c3c33adfb9aa76e80f668a988f8d
sbalasub <sbalasub@sfu.ca>
2020-07-05 14:45:00 -0700

    Routing between home and dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component'
+import { CreatecourseComponent } from './components/createcourse/createcourse.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'createcourse',
+    component: CreatecourseComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
